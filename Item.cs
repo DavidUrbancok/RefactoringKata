102d9b676f8df521305f351e4c0907edff53cc34
David Urbancok <urbancok.david@gmail.com>
2018-05-04 13:12:42 +0200

    Refactor code to improve readability - no tests or logical refactoring yet.

--- Item.cs
+++ Item.cs
@@ -2,8 +2,19 @@
 {
     public class Item
     {
+        /// <summary>
+        /// Item's name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Number of days left to sell the item.
+        /// </summary>
         public int SellIn { get; set; }
+
+        /// <summary>
+        /// Remaining quality indicator of an item.
+        /// </summary>
         public int Quality { get; set; }
     }
 }
